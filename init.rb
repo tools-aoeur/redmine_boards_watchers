16cc434eb47b723b94865e1238e1126118c6a4e7
Hare Ram Rai <hareram.rai@alpsalpine.com>
2023-05-08 13:49:52 +0200

    fix: Redmine 5 compatibility and code cleanup

--- init.rb
+++ init.rb
@@ -1,37 +1,32 @@
 #!/bin/env ruby
-# encoding: utf-8
-require 'redmine'
-require 'dispatcher' unless Rails::VERSION::MAJOR >= 3
-require 'bw_asset_helpers'
 
-unless Redmine::Plugin.registered_plugins.keys.include?(BW_AssetHelpers::PLUGIN_NAME)
-  Redmine::Plugin.register BW_AssetHelpers::PLUGIN_NAME do
+require 'redmine'
+require_relative 'lib/bw_asset_helpers'
+require_relative 'lib/boards_watchers/patches'
+
+unless Redmine::Plugin.registered_plugins.keys.include?(BwAssetHelpers::PLUGIN_NAME)
+  Redmine::Plugin.register BwAssetHelpers::PLUGIN_NAME do
     name 'Extended watchers management and sticky priority levels add-on'
     author 'Vitaly Klimov, Kim Pepper, Mikołaj Milej'
-    author_url 'mailto:vitaly.klimov@snowbirdgames.com'
+    url 'https://github.com/tools-aoeur/redmine_boards_watchers'
     description 'Plugin creates three levels of sticky messages and allows managing of forums/topics/wikis watchers'
-    version '0.2.7'
-    requires_redmine :version_or_higher => '1.3.0'
+    version '1.0.0'
+    requires_redmine version_or_higher: '5.0.0'
 
     project_module :boards do
-      permission :delete_board_watchers, {:boards_watchers => [:manage] }, :require => :member
-      permission :delete_message_watchers, {:boards_watchers => [:manage_topic, :manage_topic_remote] }, :require => :member
+      permission :delete_board_watchers, { boards_watchers: [:manage] }, require: :member
+      permission :delete_message_watchers, { boards_watchers: %i[manage_topic manage_topic_remote] },
+                 require: :member
     end
 
     project_module :wiki do
-      permission :delete_wiki_watchers, {:boards_watchers => [:manage_wiki] }, :require => :member
+      permission :delete_wiki_watchers, { boards_watchers: [:manage_wiki] }, require: :member
+    end
+
+    project_module :issue_tracking do
+      permission :delete_issue_watchers_bw, { boards_watchers: %i[issues_watchers_bulk watch_bulk_issues] }
     end
   end
 
-  require 'boards_watchers_hooks'
-end
-
-if Rails::VERSION::MAJOR >= 3
-  ActionDispatch::Callbacks.to_prepare do
-    require 'boards_watchers_patches'
-  end
-else
-  Dispatcher.to_prepare BW_AssetHelpers::PLUGIN_NAME do
-    require_dependency 'boards_watchers_patches'
-  end
+  require_relative 'lib/boards_watchers_hooks'
 end
---
+++ lib/boards_watchers/patches.rb
@@ -0,0 +1,103 @@
+require_dependency 'application_helper' if ENV['RAILS_ENV'] == 'production'
+require 'message'
+
+module BoardsWatchers::Patches
+  module StickyPriorityMessagePatch
+    def self.included(base) # :nodoc:
+      base.send(:include, InstanceMethods)
+      base.class_eval do
+        unloadable
+
+        if (Redmine::VERSION.to_a[0..2] <=> [1, 3, 3]) < 0
+          attr_protected :sticky_priority
+        else
+          safe_attributes 'sticky_priority',
+                          if: ->(message, user) { user.allowed_to?(:edit_messages, message.project) }
+        end
+      end
+    end
+
+    module InstanceMethods
+      def sticky_priority=(arg)
+        if sticky?
+          new_priority = arg.to_i
+          new_priority = 1 if new_priority == 0
+          write_attribute :sticky_priority, new_priority
+        else
+          write_attribute :sticky_priority, 0
+        end
+      end
+
+      def sticky_priority
+        sp = read_attribute(:sticky_priority) || 0
+        sp = 1 if sp == 0 && sticky?
+        sp
+      end
+    end
+  end
+
+  module ApplicationHelperPatch
+    def self.included(base) # :nodoc:
+      base.send(:include, InstanceMethods)
+      base.class_eval do
+        unloadable
+
+        alias_method :render_page_hierarchy_without_watchers, :render_page_hierarchy
+        alias_method :render_page_hierarchy, :render_page_hierarchy_with_watchers
+      end
+    end
+
+    module InstanceMethods
+      def render_page_hierarchy_with_watchers(pages, node = nil, options = {})
+        content = ''
+        if pages[node]
+          content << "<ul class=\"pages-hierarchy\">\n"
+          pages[node].each do |page|
+            content << '<li>'
+            content << link_to(h(page.pretty_title), { controller: 'wiki', action: 'show', project_id: page.project, id: page.title },
+                               title: (options[:timestamp] && page.updated_on ? l(:label_updated_time, distance_of_time_in_words(Time.now, page.updated_on)) : nil))
+            if authorize_for('boards_watchers', 'manage_wiki')
+              content << link_to("(#{page.watcher_users.size})",
+                                 { controller: 'boards_watchers', action: 'manage_wiki', project_id: @project, id: page.title }, class: (page.watcher_users.size > 0 ? 'icon icon-fav' : 'icon icon-fav-off'))
+            end
+            content << "\n" + render_page_hierarchy(pages, page.id, options) if pages[page.id]
+            content << "</li>\n"
+          end
+          content << "</ul>\n"
+        end
+        content.html_safe
+      end
+    end
+  end
+
+  if Redmine::VERSION::MAJOR >= 5
+    module MessagesControllerPatch
+      def self.included(base) # :nodoc:
+        base.send(:include, BoardsWatchers::MessagesActions)
+        base.class_eval do
+          unloadable
+          alias_method :new_without_watchers, :new
+          alias_method :new, :new_with_watchers
+
+          alias_method :edit_without_watchers, :edit
+          alias_method :edit, :edit_with_watchers
+
+          alias_method :reply_without_watchers, :reply
+          alias_method :reply, :reply_with_watchers
+        end
+      end
+    end
+  end
+end
+
+unless Message.included_modules.include? BoardsWatchers::Patches::StickyPriorityMessagePatch
+  Message.include BoardsWatchers::Patches::StickyPriorityMessagePatch
+end
+
+if Rails::VERSION::MAJOR >= 6 && Rails.env.production? && !(ApplicationHelper.included_modules.include? BoardsWatchers::Patches::ApplicationHelperPatch)
+  ApplicationHelper.include BoardsWatchers::Patches::ApplicationHelperPatch
+end
+
+if Redmine::VERSION::MAJOR >= 5 && !(MessagesController.included_modules.include? BoardsWatchers::Patches::MessagesControllerPatch)
+  MessagesController.include BoardsWatchers::Patches::MessagesControllerPatch
+end
